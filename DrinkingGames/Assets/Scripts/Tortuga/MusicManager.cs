f2c090197059b430b0be7328881b7a8c99804cab
Sam <seidensj@gmail.com>
2016-02-11 18:37:49 -0500

    music switching

--- DrinkingGames/Assets/Scripts/Tortuga/MusicManager.cs
+++ DrinkingGames/Assets/Scripts/Tortuga/MusicManager.cs
@@ -9,30 +9,45 @@ public class MusicManager : MonoBehaviour {
 	[SerializeField] private AudioSource _audSource3;
 	[SerializeField] private AudioSource _audSource4;
 	[SerializeField] private AudioSource _audSource5;
+	[SerializeField] private AudioSource _currentAudSource;
+
+	public int prevHighestScore;
+	public int highestScore;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+		_currentAudSource = _audSource1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	
+
 		if (Input.GetKeyDown(KeyCode.S)) {
-
-			LeanTween.value(gameObject, _audSource1.volume, 0f, _audSource1.clip.length ).setOnUpdate( 
-				(float v)=>{
-					_audSource1.volume = v;
-				}
-			);
-
-			LeanTween.value(gameObject, _audSource2.volume, 1f, _audSource1.clip.length ).setOnUpdate( 
-				(float v)=>{
-					_audSource2.volume = v;
-				}
-			);
-				
 			
+			SwitchClip (_currentAudSource, _audSource2);
 		}
+
+	}
+
+	void SwitchClip(AudioSource currAudSource, AudioSource nextAudSource) {
+		currAudSource.loop = false;
+
+		bool switchingClip = true;
+		while (switchingClip) {
+			print ("switching");
+			if (currAudSource.time >= currAudSource.clip.length - 0.01f) {
+				print ("CLIP SWITCHED!");
+				currAudSource.volume = 0;
+				nextAudSource.volume = 1;
+				currAudSource.loop = true;
+				_currentAudSource = nextAudSource;
+				switchingClip = false;
+			}
+		}
+
 	}
 
 
--- DrinkingGames/Assets/Scripts/Tortuga/TortugaScoreManager.cs
+++ DrinkingGames/Assets/Scripts/Tortuga/TortugaScoreManager.cs
@@ -4,13 +4,14 @@ using System.Collections;
 using TMPro;
 public class TortugaScoreManager : MonoBehaviour {
 
+	private MusicManager _musicManager; 
 	public int blueScore = 0;
 	public int greenScore = 0;
-
 	private TextMeshProUGUI scoreText;
 
 	void Awake() {
 		DontDestroyOnLoad(gameObject);
+		_musicManager = GameObject.Find ("MusicManager").GetComponent<MusicManager> ();
 	}
 
 	
@@ -20,14 +21,16 @@ public class TortugaScoreManager : MonoBehaviour {
 	}
 
 	void OnLevelWasLoaded(int level) {
-
-		if (level == 4) {
-			
-		}
-
 		if (SceneManager.GetActiveScene().name == "TortugaScoreboard") {
 			scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>();
 			scoreText.text = blueScore + " " + greenScore;
+			setMusic ();
 		}
+	}
+
+	void setMusic() {
+
+
+		
 	}
 }
