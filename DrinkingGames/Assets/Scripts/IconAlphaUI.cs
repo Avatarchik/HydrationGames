d69609997c9893787351e77f5658a2b6b6cefeef
Sam <seidensj@gmail.com>
2016-04-14 16:53:50 -0400

    more cc

--- DrinkingGames/Assets/Scripts/IconAlphaUI.cs
+++ DrinkingGames/Assets/Scripts/IconAlphaUI.cs
@@ -2,7 +2,8 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class IconAlphaUI : MonoBehaviour {
+public class IconAlphaUI : MonoBehaviour
+{
 
 	[SerializeField] CircuitSignalUI _circuitSignalUI;
 	[SerializeField] private Image _image;
@@ -11,31 +12,42 @@ public class IconAlphaUI : MonoBehaviour {
 	private Color fadedColor;
 	public string input;
 
-	//	
+	//
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		fadedColor = new Color (originalColor.r, originalColor.g, originalColor.b, .3f);
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 
-		//		if (Input.GetKey(input)) {
-		//			circuitSignal.countdown = 5;
-		//			_spriteRenderer.color = originalColor;
-		//		} else {
-		//			_spriteRenderer.color = fadedColor;
-		//		}
+		if (Input.GetKey (input)) {
+			turnOn ();
+			resetCircuitSignalTimer ();
+//					circuitSignal.countdown = 5;
+//					_spriteRenderer.color = originalColor;
+		} else {
+			turnOff ();
+//					_spriteRenderer.color = fadedColor;
+		}
 
 	}
 
-	public void turnOn() {
-		_circuitSignalUI.countdown = 5;
+	public void turnOn ()
+	{
 		_image.color = originalColor;
 	}
 
-	public void turnOff() {
+	public void turnOff ()
+	{
 		_image.color = fadedColor;
+
+	}
+
+	void resetCircuitSignalTimer() {
+		_circuitSignalUI.countdown = 5;
 
 	}
 }
